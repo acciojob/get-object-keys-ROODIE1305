fe0eee643dc48257c618bf1edf685859f6ac22be
Rudra Khandelwal <135542112+ROODIE1305@users.noreply.github.com>
2026-04-06 16:06:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const student = {
   city: "Delhi"
 }
 
-function getKeys(obj) {
-  return Object.keys(obj)
+Object.prototype.getKeys = function() {
+  return Object.keys(this)  
 }
 
 console.log(getKeys(student))
